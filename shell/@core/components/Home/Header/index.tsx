0b2b2885751019e624b61d0ed4c7c9fb361867b5
Maria Luiza Pereira <maluupereiraa@gmail.com>
2025-01-05 17:06:59 -0300

    Update shell/@core/components/Home/Header/index.tsx
    Co-authored-by: Thiago F. Martins <thiagofm33@gmail.com>

--- shell/@core/components/Home/Header/index.tsx
+++ shell/@core/components/Home/Header/index.tsx
@@ -52,9 +52,6 @@ export default function Header() {
   const handleCadastroForm = async (formData: any) => {
     // TODO: function Cadastro Form
     if (Object.values(formData).indexOf("") === -1) {
-      // const token: string = session?.user.result.token;
-      // const user: any = jwtDecode(token);
-      // console.log(formData);
       const formattedFormData: any = {
         // ...formData,
         username: formData.name,
