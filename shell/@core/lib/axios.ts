1b32aa31efa706fbc398398b9b308af8bf931df9
Thiago Martins <thiagofm33@gmail.com>
2025-01-07 20:56:26 -0300

    Updated `shell/@core/lib/axios.ts`

--- shell/@core/lib/axios.ts
+++ shell/@core/lib/axios.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: process.env.NEXTAUTH_URL,
+  baseURL: process.env.NEXT_PUBLIC_NEXTAUTH_URL,
   headers: { "Content-Type": "application/json" },
 });
 
 export const axiosAuth = axios.create({
-  baseURL: process.env.NEXTAUTH_URL,
+  baseURL: process.env.NEXT_PUBLIC_NEXTAUTH_URL,
   headers: { "Content-Type": "application/json" },
 });
