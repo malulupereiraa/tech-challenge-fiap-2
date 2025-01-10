cf800cb8897aba5325edf8617166bb18d467b32d
Thiago Martins <thiagofm33@gmail.com>
2025-01-09 22:11:29 -0300

    Updated `api/src/config/db.js`

--- api/src/config/db.js
+++ api/src/config/db.js
@@ -4,22 +4,18 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
+console.log('XXXXXXXXXXXXXXXXXXXXXXXXXXX')
+console.log(process.env.NODE_ENV)
+console.log(process.env.MONGO_URI)
+console.log('XXXXXXXXXXXXXXXXXXXXXXXXXXX')
+
 const connectDB = async () => {
-    try {
-        if (process.env.NODE_ENV === 'development' || !process.env.MONGO_URI) {
-            // Iniciar MongoDB em memória para desenvolvimento
-            const mongod = await MongoMemoryServer.create();
-            const mongoUri = mongod.getUri();
-            await mongoose.connect(mongoUri);
-            console.log('Conectado ao MongoDB em memória');
-          } else {
-            // Conectar ao MongoDB real em produção
-            await mongoose.connect(process.env.MONGO_URI, {
-              useNewUrlParser: true,
-              useUnifiedTopology: true
-            });
-            console.log('Conectado ao MongoDB');
-          }
+  try {
+    // Iniciar MongoDB em memória para desenvolvimento
+    const mongod = await MongoMemoryServer.create();
+    const mongoUri = mongod.getUri();
+    await mongoose.connect(mongoUri);
+    console.log('Conectado ao MongoDB em memória');
   } catch (error) {
     console.error('Erro de Conexão do MongoDB:', error);
     process.exit(1);
