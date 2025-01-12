a72d9c3e2206ff2ef86c1a3c94db132c445a2531
Thiago F. Martins <thiagofm33@gmail.com>
2025-01-12 13:44:51 -0300

    Update api/src/index.js

--- api/src/index.js
+++ api/src/index.js
@@ -14,7 +14,7 @@ connectDB();
 const cors = require('cors');
 
 const corsOptions = {
-  origin: ['http://localhost:3001', 'http://fase-2-shell.challengecoders.com'],
+  origin: ['http://localhost:3001', 'http://localhost:3002', 'http://fase-2-shell.challengecoders.com'],
   credentials: true, // access-control-allow-credentials: true
   optionSuccessStatus: 200
 }
